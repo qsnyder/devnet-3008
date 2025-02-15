845e15c5a4811ca4eced0a518f02ffc5c3cbc670
quinn snyder <snyderq@gmail.com>
2025-02-14 17:41:44 -0700

    fixing due to provider changes

--- terraform-hcl/main.tf
+++ terraform-hcl/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     cml2 = {
-      source  = "registry.terraform.io/ciscodevnet/cml2"
+      source = "registry.terraform.io/ciscodevnet/cml2"
     }
   }
 }
@@ -21,41 +21,41 @@ resource "cml2_node" "external" {
   lab_id         = cml2_lab.clus2023.id
   nodedefinition = "external_connector"
   label          = "sbx_backend"
-  configuration  = "bridge0"
-  x = "-400"
-  y = "-160"
+  configuration  = "System Bridge"
+  x              = "-400"
+  y              = "-160"
 }
 
 resource "cml2_node" "breakout_switch" {
   lab_id         = cml2_lab.clus2023.id
   nodedefinition = "unmanaged_switch"
   label          = "breakout_switch"
-  x = "-400"
-  y = "-40"
+  x              = "-400"
+  y              = "-40"
 }
 
 resource "cml2_node" "r1" {
   lab_id         = cml2_lab.clus2023.id
   label          = "R1"
   nodedefinition = "iosv"
-  x = "-120"
-  y = "-80"
+  x              = "-120"
+  y              = "-80"
 }
 
 resource "cml2_node" "r2" {
   lab_id         = cml2_lab.clus2023.id
   label          = "R2"
   nodedefinition = "iosv"
-  x = "-200"
-  y = "80"
+  x              = "-200"
+  y              = "80"
 }
 
 resource "cml2_node" "r3" {
   lab_id         = cml2_lab.clus2023.id
   label          = "R3"
   nodedefinition = "iosv"
-  x = "-40"
-  y = "80"
+  x              = "-40"
+  y              = "80"
 }
 
 resource "cml2_link" "ext_2_us" {
@@ -117,8 +117,9 @@ resource "cml2_lifecycle" "clus2023" {
     cml2_link.r2_2_r3.id,
   ]
   configs = {
-    "R1": file("r1.info")
-    "R2": file("r2.info")
-    "R3": file("r3.info")
+    "R1" : file("r1.info")
+    "R2" : file("r2.info")
+    "R3" : file("r3.info")
   }
 }
+
